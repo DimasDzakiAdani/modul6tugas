e19f45e0744d1623b00d666db31d47283ddf2bf4
DimasDzakiAdani <127371756+DimasDzakiAdani@users.noreply.github.com>
2023-12-13 10:03:53 +0700

    Add files via upload

---
+++ test/data/api_service_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:ngelu/fetchdata/api_model.dart';
+import 'package:ngelu/fetchdata/api_service.dart';
+
+import 'api_service_test.mocks.dart';
+
+// Generate a MockClient using the Mockito package.
+// Create new instances of this class in each test.
+@GenerateMocks([http.Client])
+void main() {
+  group('fetchPosts', () {
+    test('returns an Posts if the http call completes successfully', () async {
+      final client = MockClient();
+
+      // Use Mockito to return a successful response when it calls the
+      // provided http.Client.
+      when(client
+          .get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1')))
+          .thenAnswer((_) async =>
+          http.Response('{"userId": 2, "id": 2, "title": "Test Title", "body": "Test Body"}', 200));
+
+      expect(await fetchPosts(client), isA<Posts>());
+    });
+
+    test('throws an exception if the http call completes with an error', () {
+      final client = MockClient();
+
+      // Use Mockito to return an unsuccessful response when it calls the
+      // provided http.Client.
+      when(client
+          .get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1')))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(fetchPosts(client), throwsException);
+    });
+  });
+}
